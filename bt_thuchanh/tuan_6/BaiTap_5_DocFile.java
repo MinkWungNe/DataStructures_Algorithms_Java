d19edada8ad133a507b1e09445b2d294c54b0fb6
MinkWungNe <devilmask2005@gmail.com>
2024-10-11 18:37:10 +0700

    ThucHanh_6

---
+++ bt_thuchanh/tuan_6/BaiTap_5_DocFile.java
@@ -0,0 +1,26 @@
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+
+public class BaiTap_5_DocFile {
+    public static void main(String[] args) throws FileNotFoundException {
+        String filePath = System.getProperty("user.dir") + File.separator + "tally.txt";
+        Scanner console = new Scanner(new File(filePath));
+        
+        int counter[] = new int[5];   // Mảng chứa để đếm số
+
+        // Chạy khi file còn số
+        while (console.hasNextInt())
+        {
+            int result = console.nextInt();
+            counter[result]++;
+        }
+
+        System.out.println("Ket qua kiem tra file: ");
+        for (int i = 0; i < counter.length ; i++)
+        {
+            System.out.println(i + ": " + counter[i]);
+        }
+
+    }
+}
