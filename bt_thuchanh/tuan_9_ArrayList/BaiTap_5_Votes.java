10da479976a249563ee63bf2299634842ffd4875
MinkWungNe <leluongminhquan@gmail.com>
2024-10-18 17:34:57 +0700

    Tuan_9

--- bt_thuchanh/tuan_9_ArrayList/BaiTap_5_Votes.java
+++ bt_thuchanh/tuan_9_ArrayList/BaiTap_5_Votes.java
@@ -2,14 +2,36 @@
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.Scanner;
 
 public class BaiTap_5_Votes {
     public static void main(String[] args) throws FileNotFoundException{
         ArrayList<String> UngVien = new ArrayList<>();
-        ArrayList<Integer> PhieuBau = new ArrayList<>();
+        ArrayList<String> PhieuBau = new ArrayList<>();
         String filePath = System.getProperty("user.dir") + File.separator + "phieu.txt";
-        Scanner console = new Scanner(new File(filePath));
+        try (Scanner console = new Scanner(new File(filePath))) {
+            while (console.hasNext())
+            {
+                String name = console.next();
+                PhieuBau.add(name);
+
+                // Bổ sung tên ứng viên nếu chưa có
+                if (!UngVien.contains(name))    
+                {
+                    UngVien.add(name);
+                }
+            }
+        }
+
+        System.out.println("list = " + UngVien);
+
+        // Sort tên ứng viên
+        Collections.sort(UngVien);
+
+        // Đếm phiếu
+        int VotesCount[] = new int[4];
+
         
     }
 }
