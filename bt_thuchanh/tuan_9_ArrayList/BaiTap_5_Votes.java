954e035e0505bb9115f531a0881ed49cfa977c9f
MinkWungNe <leluongminhquan@gmail.com>
2024-10-18 17:26:06 +0700

    Tuan_9

---
+++ bt_thuchanh/tuan_9_ArrayList/BaiTap_5_Votes.java
@@ -0,0 +1,15 @@
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class BaiTap_5_Votes {
+    public static void main(String[] args) throws FileNotFoundException{
+        ArrayList<String> UngVien = new ArrayList<>();
+        ArrayList<Integer> PhieuBau = new ArrayList<>();
+        String filePath = System.getProperty("user.dir") + File.separator + "phieu.txt";
+        Scanner console = new Scanner(new File(filePath));
+        
+    }
+}
