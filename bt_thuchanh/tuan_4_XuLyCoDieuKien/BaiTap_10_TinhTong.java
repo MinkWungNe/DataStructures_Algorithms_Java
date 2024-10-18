61c45fc9bfb75b69059687f58e46097f310dd1bd
MinkWungNe <leluongminhquan@gmail.com>
2024-10-18 17:24:34 +0700

    Tuan_4

--- bt_thuchanh/tuan_4_XuLyCoDieuKien/BaiTap_10_TinhTong.java
+++ bt_thuchanh/tuan_4_XuLyCoDieuKien/BaiTap_10_TinhTong.java
@@ -2,19 +2,18 @@ import java.util.Scanner;
 
 public class BaiTap_10_TinhTong {
     public static void main(String[] args) {
-        Scanner console = new Scanner(System.in);
-        double sum = 0.0;
-
-        System.out.print("Nhap n: ");
-        int n = console.nextInt();
-
-        for (int i = 0; i <= n; i++)
-        {
-            sum += 1.0/i;
+        try (Scanner console = new Scanner(System.in)) {
+            double sum = 0.0;
+            
+            System.out.print("Nhap n: ");
+            int n = console.nextInt();
+            
+            for (int i = 1; i <= n; i++)
+            {
+                sum += 1.0/i;
+            }
+            
+            System.out.println("Ket qua: " + sum);
         }
-        
-        System.out.println("Ket qua: " + sum);
-
-        console.close();
     }
 }
