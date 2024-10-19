a86392b9f777a5fc3bbd42322066b69f3c860590
MinkWungNe <devilmask2005@gmail.com>
2024-10-19 11:06:49 +0700

    Tuan_8

--- bt_thuchanh/tuan_8_Class-KeThuaVaGiaoTiep/Employee.java
+++ bt_thuchanh/tuan_8_Class-KeThuaVaGiaoTiep/Employee.java
@@ -3,24 +3,28 @@ public class Employee
     private int hour;
     private int salary;
     private int VacationDays;
-    private String form = "yellow"; // Tượng trưng "biểu mẫu màu vàng" :L
+    private String form;
 
-    public Employee(int hour, int salary, int VacationDays) // Khởi tạo có giá trị
+    // Khởi tạo mặc định
+    public Employee()   
+    {
+        this(40, 40000, 10, "yellow");    // 2 tuần nghỉ phép (không tính T7, CN)
+    }
+
+    // Khởi tạo có giá trị
+    public Employee(int hour, int salary, int VacationDays, String form) 
     {
         this.hour = hour;
         this.salary = salary;
         this.VacationDays = VacationDays;
+        this.form = form;
     }
 
-    public Employee()   // Khởi tạo mặc định
-    {
-        this(0, 0, 0);
-    }
-
+    // Hàm lấy thông tin
     public int getHours() { return this.hour; }
     public int getSalary() { return this.salary; }
     public int getVacationDays() { return this.VacationDays; }
     public String getVacationForm() { return this.form; } 
 }
 
-// Bài Tập Số 1.
+// Bài Tập Số 1: viết lớp Employee
