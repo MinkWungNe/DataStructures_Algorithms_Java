14c48db6ae2b0030b409cd3b602b1fea8e2531eb
MinkWungNe <leluongminhquan@gmail.com>
2024-10-22 09:46:18 +0700

    tuan_9

--- bt_thuchanh/tuan_10_LinkedList/BaiTap_4_HashSet_DocFile.java
+++ bt_thuchanh/tuan_10_LinkedList/BaiTap_4_HashSet_DocFile.java
@@ -1,3 +1,13 @@
+import java.util.HashSet;
+import java.util.Set;
+
 public class BaiTap_4_HashSet_DocFile {
-    
+    public static void main(String[] args) {
+        Set<String> set = new HashSet<>();
+    }
+
+    public static void ReadFile()
+    {
+        
+    }
 }
