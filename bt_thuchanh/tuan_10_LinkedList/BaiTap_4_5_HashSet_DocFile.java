3dcdd531fcad75c759ff7ab365ba296f9055b5ac
MinkWungNe <leluongminhquan@gmail.com>
2024-10-22 09:58:58 +0700

    tuan_10

--- bt_thuchanh/tuan_10_LinkedList/BaiTap_4_5_HashSet_DocFile.java
+++ bt_thuchanh/tuan_10_LinkedList/BaiTap_4_5_HashSet_DocFile.java
@@ -1,9 +1,12 @@
 import java.util.HashSet;
+import java.util.Scanner;
 import java.util.Set;
 
 public class BaiTap_4_5_HashSet_DocFile {
     public static void main(String[] args) {
+        Scanner console = new Scanner("mission.txt");
         Set<String> set = new HashSet<>();
+        
     }
 
     public static void ReadFile()
