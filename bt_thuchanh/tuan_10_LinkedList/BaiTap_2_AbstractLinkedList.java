3b64b68a9970810bd2b371427a01227bad1c52a8
MinkWungNe <leluongminhquan@gmail.com>
2024-10-25 17:30:40 +0700

    Tuan_10

--- bt_thuchanh/tuan_10_LinkedList/BaiTap_2_AbstractLinkedList.java
+++ bt_thuchanh/tuan_10_LinkedList/BaiTap_2_AbstractLinkedList.java
@@ -1,48 +1,105 @@
-
+import java.util.Collections;
+import java.util.Iterator;
 import java.util.LinkedList;
 import java.util.List;
 
 public class BaiTap_2_AbstractLinkedList {
     public static void main(String[] args) {
+        // 1
         List<Integer> list = new LinkedList<>(List.of(10, 20, 30, 40));
+        PrintList(list);
 
+        // 2
+        List<Integer> templist = DeleteFirst(list);
+        PrintList(templist);
+
+        // 3
+        templist = AddFirst(list, 50);
+        PrintList(templist);
+
+        // 4
+        templist = Sort(list);
+        PrintList(list);
+
+        // 5
+        int result = FindMax(list);
+        System.out.println("Max: " + result);
+
+        // 6
+        result = FindMin(list);
+        System.out.println("Min: " + result);
+
+        // 7
+        templist = Sort(list);
+        PrintList(templist);
     }
 
-    public static LinkedList<Integer> AddFirst(LinkedList<Integer> list, int IntegerToAdd)
+    public static void PrintList(List<Integer> list)
     {
-        LinkedList<Integer> tempList = new LinkedList<>(List.copyOf(list));
+        Iterator<Integer> i = list.iterator();
+        while (i.hasNext())
+        {
+            int element = i.next();
+            System.out.print(element + " ");
+        }
+        System.out.println();
+    }
+
+    public static List<Integer> AddFirst(List<Integer> list, int IntegerToAdd)
+    {
+        List<Integer> tempList = new LinkedList<>(List.copyOf(list));
         tempList.add(0, IntegerToAdd);
         return tempList;
     }
 
-    public static LinkedList<Integer> DeleteFirst(LinkedList<Integer> list)
+    public static List<Integer> DeleteFirst(List<Integer> list)
     {
-        LinkedList<Integer> tempList = new LinkedList<>(List.copyOf(list));
-
+        List<Integer> tempList = new LinkedList<>(List.copyOf(list));
+        tempList.remove(0);
         return tempList;
     }
 
-    public static LinkedList<Integer> Sort(LinkedList<Integer> list)
+    public static List<Integer> Sort(List<Integer> list)
     {
-        LinkedList<Integer> tempList = new LinkedList<>(List.copyOf(list));
-
+        List<Integer> tempList = new LinkedList<>(List.copyOf(list));
+        Collections.sort(tempList);
         return tempList;
     }
 
-    public static void FindMax(LinkedList<Integer> list)
+    public static int FindMax(List<Integer> list)
     {
-        
+        int max = 0;
+        Iterator<Integer> i = list.iterator();
+        while (i.hasNext())
+        {
+            int element = i.next();
+            if (element > max)
+            {
+                max = element;
+            }
+        }
+        return max;
     }
 
-    public static void FindMin(LinkedList<Integer> list)
+    public static int FindMin(List<Integer> list)
     {
-
+        int min = 0;
+        Iterator<Integer> i = list.iterator();
+        while (i.hasNext())
+        {
+            int element = i.next();
+            if (element < min)
+            {
+                min = element;
+            }
+        }
+        return min;
     }
 
-    public static LinkedList<Integer> Swap(LinkedList<Integer> list)
+    public static List<Integer> Swap(List<Integer> list, int index1, int index2)
     {
-        LinkedList<Integer> tempList = new LinkedList<>(List.copyOf(list));
-
+        List<Integer> tempList = new LinkedList<>(List.copyOf(list));
+        Collections.swap(tempList, index1, index2);
         return tempList;
     }
 }
--- bt_thuchanh/tuan_10_LinkedList/BaiTap_3_SieveOfEratosthenes.java
+++ bt_thuchanh/tuan_10_LinkedList/BaiTap_3_SieveOfEratosthenes.java
@@ -7,18 +7,27 @@ public class BaiTap_3_SieveOfEratosthenes {
     public static void main(String[] args) {
         // 1) Tạo dãy liên tục (2 -> 25)
         List<Integer> list = Initialize(2, 25);
-        PrintList(list);
+        List<Integer> soNguyenTo = new LinkedList<>();
+        PrintList(list, "Day so");
+        PrintList(soNguyenTo, "So Nguyen To");
 
+        while (!list.isEmpty())
+        {   
+            int PrimeNum = GetPrime(list, soNguyenTo);
+            Clean(list, PrimeNum);
+            PrintList(list, "Day so");
+            PrintList(soNguyenTo, "So Nguyen To");
+            System.out.println();
+        }
 
     }
-
 
     // Tạo dãy số
     public static List<Integer> Initialize(int Start, int Max)
     {
         List<Integer> tempList = new LinkedList<>();
 
-        for (int i = Start; i < Max; i++)
+        for (int i = Start; i <= Max; i++)
         {
             tempList.add(i);
         }
@@ -27,19 +36,30 @@ public class BaiTap_3_SieveOfEratosthenes {
     }
 
     // Lấy số nguyên tố
-    public static void GetPrime(List<Integer> list)
-    {
-        
+    public static int GetPrime(List<Integer> list, List<Integer> soNguyenTo)
+    {   
+        soNguyenTo.add(list.get(0));
+        return list.get(0);
     }
 
-    public static void Clean(List<Integer> list)
+    // Xóa bớt các số chia hết cho số nguyên tố
+    public static void Clean(List<Integer> list, int Prime)
     {
-
+        Iterator<Integer> i = list.iterator();
+        while(i.hasNext())  // loop tới rỗng
+        {
+            int element = i.next();
+            if (element % Prime == 0)
+            {
+                i.remove();
+            }
+        }
     }
 
     // Print
-    public static void PrintList(List<Integer> list)
-    {
+    public static void PrintList(List<Integer> list, String ListName)
+    {   
+        System.out.print("[" + ListName + "]: ");   // In tên List
         Iterator<Integer> i = list.iterator();
         while (i.hasNext())
         {
